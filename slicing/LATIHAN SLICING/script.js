df3d7e934e41926d2e9f0226bec66af16ab695d1
barakaramadhan <barakaramadhan444@gmail.com>
2024-10-24 10:18:57 +0700

    index

---
+++ slicing/LATIHAN SLICING/script.js
@@ -0,0 +1,10 @@
+alert('Selamat Datang Di Ponpes Abu Dzar');
+document.addEventListener("DOMContentLoaded", function() {
+    const visitWebsiteButton = document.getElementById("visitWebsite");
+    visitWebsiteButton.addEventListener("click", function() {
+        window.location.href = "https://abudzar.ponpes.id/";
+    });
+});
+
+alert("Assala/mu'alaikum");
+alert(3 + 2);
---
+++ slicing/WEBSITE SENDIRI/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const visitWebsiteButton = document.getElementById("visitWebsite");
+    visitWebsiteButton.addEventListener("click", function() {
+        window.location.href = "https://abudzar.ponpes.id/";
+    });
+});
