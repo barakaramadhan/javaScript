31571f6e6e2e86c32ebe76acb934fdd7846b9e31
barakaramadhan <barakaramadhan444@gmail.com>
2025-01-10 08:35:19 +0700

    tugas 6

---
+++ pertemuan 5/script.js
@@ -0,0 +1,15 @@
+// LATIHAN SOAL NOMOR 1
+let dressSize = 45;
+console.log(typeof dressSize);
+
+dressSize = String(dressSize);
+console.log(typeof dressSize);
+
+
+// LATIHAN SOAL NOMOR 2
+let isLight = true;
+console.log(typeof isLight);
+
+isLight = String(isLight);
+console.log(typeof isLight);
+
